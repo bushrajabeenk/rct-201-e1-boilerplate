b40dd10e0576b4007973930200bd4a0edb2f6925
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-11 16:37:27 +0530

    ch

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,26 +1,38 @@
 import React from "react";
+import useClock from "../hooks/useClock";
 
 type Props = {
   //  TODO
+  hours: number;
+  minutes: number;
+  seconds: number;
+
 };
 
 const Clock = (props: Props) => {
+  const { hours, minutes, seconds }=useClock();
+
+
   return (
     <div data-testid="clock">
       <h4 data-testid="clock-label">
         {/* Label */}
+        24 hours Live Custom Component Clock
       </h4>
       <span data-testid="clock-hours">
         {/* Hours */}
-        </span>
-        :
+        {hours}
+      </span>
+      :
       <span data-testid="clock-minutes">
         {/* Minutes */}
+        {minutes}
       </span>
-       :
+      :
       <span data-testid="clock-seconds">
         {/* Seconds */}
-        </span>
+        {seconds}
+      </span>
     </div>
   );
 };
--- src/hooks/useClock.tsx
+++ src/hooks/useClock.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+
 const useClock = () => {
   // TODO
   // refer readme.md for what to return
+  const [clock, setClock] = useState(new Date());
+
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      setClock(new Date());
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  const hours = clock.getHours();
+  const minutes = clock.getMinutes();
+  const seconds = clock.getSeconds();
+  return { hours, minutes, seconds }
 };
 
 export default useClock;
